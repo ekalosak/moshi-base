10ec52124577a8cbc5e50aad1c74a1a2516a8069
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-29 14:13:55 -0400

    tests passing

--- src/moshibase/__init__.py
+++ src/moshibase/__init__.py
@@ -1,4 +1,4 @@
 from .log import setup_loguru
 from .msg import Message, Role
-from .model import Model, ModelType, CompletionM, ChatM
+from .model import CompletionM, ChatM
 from .versioned import __version__, Versioned
--- src/moshibase/model.py
+++ src/moshibase/model.py
@@ -1,28 +1,18 @@
-"""Datamodels of GPT model configurations."""
+"""Datamodels of GPT model configurations.
+Source:
+    - https://platform.openai.com/docs/api-reference/models
+"""
 from enum import Enum
 
-class ModelType(str, Enum):
-    """The two model types used by this app.
-    Source:
-        - https://platform.openai.com/docs/api-reference/models
-    """
-    COMP = "completion"
-    CHAT = "chat_completion"
-
-class Model(Enum, str):
-    _type: ModelType
-
-class CompletionM(Model):
+class CompletionM(str, Enum):
     """Completion models don't use roles."""
-    _type = ModelType.COMP
     TEXTDAVINCI003 = "text-davinci-003"
     TEXTDAVINCI002 = "text-davinci-002"
     TEXTCURIE001 = "text-curie-001"
     TEXTBABBAGE001 = "text-babbage-001"
     TEXTADA001 = "text-ada-001"
 
-class ChatM(Model):
+class ChatM(str, Enum):
     """Chat completion models do use roles."""
-    _type = ModelType.CHAT
     GPT35TURBO = "gpt-3.5-turbo"
     GPT35TURBO0301 = "gpt-3.5-turbo-0301"
