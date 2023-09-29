a0707a198b0b5332b67c8ce0d6474cdd2a0c269d
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-29 12:50:13 -0400

    msgs are now moshi-type 'sys' rather than openai-type 'system' but have translation functions

--- src/moshibase/msg.py
+++ src/moshibase/msg.py
@@ -4,10 +4,24 @@ from enum import Enum
 from .audio import AudioStorage
 from .versioned import Versioned
 
+OPENAI_ROLES = {
+    "sys": "system",
+    "usr": "user",
+    "ast": "assistant",
+}
+MOSHI_ROLES = {v: k for k, v in OPENAI_ROLES.items()}
+
 class Role(str, Enum):
-    SYS = "system"
-    USR = "user"
-    AST = "assistant"
+    SYS = "sys"
+    USR = "usr"
+    AST = "ast"
+
+    def to_openai(self):
+        return OPENAI_ROLES[self.value]
+
+    @classmethod
+    def from_openai(cls, role: str):
+        return cls(MOSHI_ROLES[role])
 
 class Message(Versioned):
     role: Role
