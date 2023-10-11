b47344c9a81b56482475ac4dfea66d366eaf5839
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-10-11 18:40:14 -0400

    drafted translation in language class using google-cloud-translate

--- src/moshi/language.py
+++ src/moshi/language.py
@@ -1,16 +1,19 @@
-"""This module provides language primitives."""
-from pathlib import Path
-
-from google.cloud.firestore import DocumentSnapshot, Client, DocumentReference
-
+""" This module provides language primitives.
+The Language class wraps langcodes.Language for use with Firebase.
+The match function uses isocodes to match a language name to a language code.
+"""
 # TODO import iso639
 import isocodes  # for country annotation
+# from google.cloud.translate_v3 import TranslationServiceClient
+from google.cloud.translate_v2 import Client as TranslationClient
 import langcodes  # for language matching
 from loguru import logger
 from pydantic import Field
 
 from .storage import FB, DocPath
 from .voice import Voice
+
+tra: TranslationClient = None
 
 def match(language: str) -> str:
     """Get the closest matching language code ISO-639-1."""
@@ -21,9 +24,23 @@ def match(language: str) -> str:
         lan = isocodes.languages.get(alpha_2=language)['alpha_2']
     if not lan:
         raise ValueError(f"Could not find language for {language}")
-    # logger.debug(f"Matched {language} to {lan} using isocodes.")
+    logger.debug(f"Matched {language} to {lan} using isocodes.")
     assert len(lan) in {2, 3}, f"Invalid language code: {lan}"
     return lan
+
+def translate(text: str, target_bcp47: str, source_bcp47: str=None) -> str:
+    """ Translate text the target language. """
+    global tra
+    if tra is None:
+        logger.debug("Initializing TranslationServiceClient...")
+        tra = TranslationClient()
+        logger.debug("Initialized TranslationServiceClient.")
+    logger.debug(f"Translating text to {target_bcp47}: {text}")
+    res = tra.translate(text, target_language=target_bcp47, source_language=source_bcp47)
+    with logger.contextualize(**res):
+        logger.debug(f"Translated text: {res['translatedText']}")    
+    return res['translatedText']
+
 
 class Language(FB):
     _language: langcodes.Language
@@ -34,11 +51,15 @@ class Language(FB):
 
     def __init__(self, bcp47: str, **kwargs):
         lang: langcodes.Language = langcodes.Language.get(bcp47.strip())
-        # logger.debug(f"Matched bcp47={bcp47} to {lang.language_name()}")
+        logger.debug(f"Matched bcp47={bcp47} to {lang.language_name()}")
         super().__init__(**kwargs)
         self._language = lang
         self._country: dict[str, str] = isocodes.countries.get(alpha_2=self._language.territory)
         self._bcp47 = self._language.to_tag()
+
+    def __str__(self):
+        res = f"{self.name} ({self.bcp47})"
+        return f"L<{res}>"
 
     @property
     def docpath(self) -> DocPath:
@@ -60,6 +81,5 @@ class Language(FB):
     def country(self) -> dict[str, str]:
         return self._country
 
-    def __str__(self):
-        res = f"{self.name} ({self.bcp47})"
-        return f"L<{res}>"
+    def translate(self, text: str, source_bcp47: str=None) -> str:
+        return translate(text, self.bcp47, source_bcp47)
