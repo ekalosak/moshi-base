f36625329021898ac68b849fb5ec6fa84a485556
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-10-12 16:16:13 -0400

    vocab -> FB from dataclass; revised its datamodel

--- src/moshi/vocab.py
+++ src/moshi/vocab.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel
+from pydantic import Field
+from .storage import FB
 
 
-class Vocab(BaseModel):
-    term: str
-    term_translation: str
-    part_of_speech: str = "missing"
-    definition: str = "missing"
-    definition_translation: str = "missing"
+# FB inherits pydantic.BaseModel
+class Vocab(FB):
+    """ Represents a vocabulary term.
+    """
+    bcp47: str = Field(help="BCP-47 language code.")
+    defn: str = Field(help="Definition of the term in the source language. See self.bcp47.", default=None, alias="definition")
+    pos: str = Field(help="Part of speech.", default=None, alias="part_of_speech")
+    term: str = Field(help="As used in the source utterance.")
+    translations: dict[str, 'Vocab'] = Field(help="Translation of the term. Keys are BCP-47 language codes.", default=None, alias="trs")
+    root: str = Field(help="Root form of the term. Usually provided for verbs.", examples=["For 'went' it would be 'to go'."], default=None)
+    conj: str = Field(help="Conjugation of the term. Usually provided for verbs.", examples=["For 'went' it would be 'past tense'."], default=None, alias="conjugation")
--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,4 +1,11 @@
 from moshi import Vocab
 
-def test_vocab():
-    _ = Vocab(term="test", term_translation="test", part_of_speech="test", definition="test", definition_translation="test")
+def test_vocab_init():
+    vocab = Vocab(term="test", term_translation="test", part_of_speech="test", definition="test", definition_translation="test")
+    assert vocab.term == "test"
+    assert vocab.translations == {"term_translation": None}
+    assert vocab.pos == "test"
+    assert vocab.defn == "test"
+    assert vocab.bcp47 == None
+    assert vocab.root == None
+    assert vocab.conj == None
