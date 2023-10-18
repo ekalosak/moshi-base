fa790d682a2bcee43431a21b8e47925cbc345e14
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-10-18 13:36:27 -0400

    llmfx topics works and tested

--- src/moshi/llmfx/topics.py
+++ src/moshi/llmfx/topics.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from moshi.activ import Plan, pid2plan
+from moshi.language import Language
 from moshi.llmfx.base import PROMPT_DIR
 from moshi.msg import Message
 from moshi.prompt import Prompt
@@ -16,7 +17,12 @@ if not PROMPT_FILE.exists():
 def extract(tra: Transcript) -> list[str]:
     """Get a list of topics for the given transcript."""
     pro = Prompt.from_file(PROMPT_FILE)
-    pro.template(TRANSCRIPT=tra.to_templatable())
-    topics: list[str] = pro.complete(stop=None).body.split(", ")
+    txt = '"""\n' + tra.to_templatable() + '\n"""'
+    pro.template(
+        LANGUAGE=Language(tra.bcp47).name,
+        MAX_RESPONSES='five',
+    )
+    pro.msgs.append(Message('usr', txt))
+    topics: list[str] = pro.complete().body.split(", ")
     logger.success(f"Extracted topics: {topics}")
     return topics
